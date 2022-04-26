918b438e59f76407ea2c14716b5fa775cef2007b
Nick Lu <tlu32@jhu.edu>
2022-04-25 20:59:23 -0400

    First commit

---
+++ src/main/java/hw8/spp/DijkstraStreetSearcher.java
@@ -0,0 +1,34 @@
+package hw8.spp;
+
+import hw8.graph.Edge;
+import hw8.graph.Graph;
+import hw8.graph.Vertex;
+import java.util.List;
+
+public class DijkstraStreetSearcher extends StreetSearcher {
+
+  /**
+   * Creates a StreetSearcher object.
+   *
+   * @param graph an implementation of Graph ADT.
+   */
+  public DijkstraStreetSearcher(Graph<String, String> graph) {
+    super(graph);
+  }
+
+  @Override
+  public void findShortestPath(String startName, String endName) {
+    Vertex<String> start = vertices.get(startName);
+    Vertex<String> end = vertices.get(endName);
+
+    double totalDist = -1;  // totalDist must be update below
+
+    // TODO - Implement Dijkstra Algorithm!
+
+    // These method calls will create and print the path for you
+    List<Edge<String>> path = getPath(end, start);
+    if (VERBOSE) {
+      printPath(path, totalDist);
+    }
+  }
+}
---
+++ src/test/java/hw8/GraphTest.java
@@ -0,0 +1,54 @@
+package hw8;
+
+import exceptions.PositionException;
+import hw8.graph.Edge;
+import hw8.graph.Graph;
+import hw8.graph.Vertex;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public abstract class GraphTest {
+
+  protected Graph<String, String> graph;
+
+  @BeforeEach
+  public void setupGraph() {
+    this.graph = createGraph();
+  }
+
+  protected abstract Graph<String, String> createGraph();
+
+  @Test
+  @DisplayName("insert(v) returns a vertex with given data")
+  public void canGetVertexAfterInsert() {
+    Vertex<String> v1 = graph.insert("v1");
+    assertEquals(v1.get(), "v1");
+  }
+
+  @Test
+  @DisplayName("insert(U, V, e) returns an edge with given data")
+  public void canGetEdgeAfterInsert() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Edge<String> e1 = graph.insert(v1, v2, "v1-v2");
+    assertEquals(e1.get(), "v1-v2");
+  }
+
+  @Test
+  @DisplayName("insert(null, V, e) throws exception")
+  public void insertEdgeThrowsPositionExceptionWhenfirstVertexIsNull() {
+    try {
+      Vertex<String> v = graph.insert("v");
+      graph.insert(null, v, "e");
+      fail("The expected exception was not thrown");
+    } catch (PositionException ex) {
+      return;
+    }
+  }
+
+  // TODO add more tests here.
+}
