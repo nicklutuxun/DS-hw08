2e8994d5b939e7040c9c0b77c0ac8282a77e4f81
Nick Lu <tlu32@jhu.edu>
2022-04-26 14:52:47 -0400

    Add tests

--- src/test/java/hw8/GraphTest.java
+++ src/test/java/hw8/GraphTest.java
@@ -5,6 +5,7 @@ import exceptions.PositionException;
 import exceptions.RemovalException;
 import hw8.graph.Edge;
 import hw8.graph.Graph;
+import hw8.graph.Position;
 import hw8.graph.Vertex;
 import javafx.geometry.Pos;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +32,19 @@ public abstract class GraphTest {
     Vertex<String> v1 = graph.insert("v1");
     assertEquals(v1.get(), "v1");
   }
+  
+  @Test
+  @DisplayName("insert duplicate vertices throws exception")
+  public void insertDuplicateVerticesThrowsInsertionException () {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v1");
+      fail("The expected exception was not thrown");
+    } catch (InsertionException ex) {
+      return;
+    }
+  }
+  
 
   @Test
   @DisplayName("insert(U, V, e) returns an edge with given data")
@@ -90,6 +104,7 @@ public abstract class GraphTest {
     Edge<String> e3 = graph.insert(v3, v1, "v3-v1");
     int counter = 0;
     for (Vertex<String> v : graph.vertices()) {
+      System.out.println(v.get());
       counter++;
     }
     assertEquals(counter, 3);
@@ -106,8 +121,43 @@ public abstract class GraphTest {
     Edge<String> e3 = graph.insert(v3, v1, "v3-v1");
     int counter = 0;
     for (Edge<String> e : graph.edges()) {
+      System.out.println(e.get());
       counter++;
     }
     assertEquals(counter, 3);
+  }
+  
+  @Test
+  @DisplayName("outgoing() correctly returns an iterable of all outgoing edges")
+  public void outgoingCorrectlyReturnsAnIterableOfOutgoingEdges() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Vertex<String> v3 = graph.insert("v3");
+    Edge<String> e1 = graph.insert(v1, v2, "v1-v2");
+    Edge<String> e2 = graph.insert(v2, v3, "v2-v3");
+    Edge<String> e3 = graph.insert(v3, v1, "v3-v1");
+    int counter = 0;
+    for (Edge<String> e : graph.outgoing(v1)) {
+      System.out.println(e.get());
+      counter++;
+    }
+    assertEquals(counter, 1);
+  }
+  
+  @Test
+  @DisplayName("incoming() correctly returns an iterable of all incoming edges")
+  public void incomingCorrectlyReturnsAnIterableOfIncomingEdges() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Vertex<String> v3 = graph.insert("v3");
+    Edge<String> e1 = graph.insert(v1, v2, "v1-v2");
+    Edge<String> e2 = graph.insert(v2, v3, "v2-v3");
+    Edge<String> e3 = graph.insert(v3, v1, "v3-v1");
+    int counter = 0;
+    for (Edge<String> e : graph.incoming(v1)) {
+      System.out.println(e.get());
+      counter++;
+    }
+    assertEquals(counter, 1);
   }
 }
